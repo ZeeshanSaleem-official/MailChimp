aaf0d3f63f589b4fef82715367e2dd9fac808485
Zeeshan Saleem <l1f22bsse0031@ucp.edu.pk>
2026-02-25 23:07:28 +0500

    Initial commit for the Mail Chimp Project with changes

--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Email Dispatcher using GoLang Backend!!!")
-	fmt.Println("Email Dispatcher for sending email concurrently")
+	fmt.Println("Email Dispatcher for sending email concurrently, obviosuly doing using the state of the art go routines")
 }
