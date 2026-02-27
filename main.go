f73c4fc9ebf24c9f15f9261b5e14c455ec500066
Zeeshan Saleem <l1f22bsse0031@ucp.edu.pk>
2026-02-27 23:40:45 +0500

    Initial commit for the Mail Chimp Project with changes

--- main.go
+++ main.go
@@ -6,4 +6,9 @@ func main() {
 	fmt.Println("Email Dispatcher using GoLang Backend!!!")
 	fmt.Println("Email Dispatcher for sending email concurrently, obviosuly doing using the state of the art go routines")
 	fmt.Println("This new email dispatcher very handy for the beginners, and it will be very constrcutive to make.")
+	fmt.Println("For making this dispatcher is very beneficial to get more hands dirty with the Go Language( truly a c++ on steroids)")
+	fmt.Println("This new email dispatcher very handy for the beginners, and it will be very constrcutive to make.")
+	fmt.Println("For making this dispatcher is very beneficial to get more hands dirty with the Go Language( truly a c++ on steroids)")
+	fmt.Println("This new email dispatcher very handy for the beginners, and it will be very constrcutive to make.")
+	fmt.Println("For making this dispatcher is very beneficial to get more hands dirty with the Go Language( truly a c++ on steroids)")
 }
