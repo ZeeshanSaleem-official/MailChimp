710bb3ea5614185dacbc65a0c861c5a5bf85cc9b
Zeeshan Saleem <l1f22bsse0031@ucp.edu.pk>
2026-03-02 23:55:52 +0500

    Initial commit for the Mail Chimp Project with changes

--- main.go
+++ main.go
@@ -4,11 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Email Dispatcher using GoLang Backend!!!")
-	fmt.Println("Email Dispatcher for sending email concurrently, obviosuly doing using the state of the art go routines")
-	fmt.Println("This new email dispatcher very handy for the beginners, and it will be very constrcutive to make.")
-	fmt.Println("For making this dispatcher is very beneficial to get more hands dirty with the Go Language( truly a c++ on steroids)")
-	fmt.Println("This new email dispatcher very handy for the beginners, and it will be very constrcutive to make.")
-	fmt.Println("For making this dispatcher is very beneficial to get more hands dirty with the Go Language( truly a c++ on steroids)")
-	fmt.Println("This new email dispatcher very handy for the beginners, and it will be very constrcutive to make.")
-	fmt.Println("For making this dispatcher is very beneficial to get more hands dirty with the Go Language( truly a c++ on steroids)")
+
 }
--- producer.go
+++ producer.go
@@ -1,5 +1,19 @@
 package main
 
-func producingMails() {
+import (
+	"encoding/csv"
+	"os"
+)
+
+func loadRecipients(filePath string) error {
 	// we will use the channels and much more
+	f, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	Reader := csv.NewReader(f)
+	records, err := Reader.ReadAll()
+	if err != nil {
+		return err
+	}
 }
