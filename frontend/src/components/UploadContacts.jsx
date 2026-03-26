f67bf980a3cae00fa74f51fce964d18e609a6dd8
Zeeshan Saleem <l1f22bsse0031@ucp.edu.pk>
2026-03-27 00:04:05 +0500

    Fix minor bug in  Upload csv file function in frontend

--- frontend/src/components/UploadContacts.jsx
+++ frontend/src/components/UploadContacts.jsx
@@ -90,7 +90,7 @@ export default function uploadContact({ onUploadSuccess }) {
           className={`w-full mt-6 text-white font-bold py-3 px-4 rounded-lg transition-colors shadow-md flex items-center justify-center gap-2
             ${isUploading ? 'bg-emerald-400 cursor-not-allowed' : 'bg-emerald-600 hover:bg-emerald-700 hover:shadow-lg'}`}
         >
-          ${isUploading ? 'Uploading to server': 'Uploaded to Database'}
+          {isUploading ? 'Uploading to server': 'Upload to Database'}
           </button>
         )}
         {uploadMessage && (
